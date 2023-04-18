662cff54106b4c97c9818cee856d9e33cdbc3eec
beattris <beatriceebirim@gmail.com>
2023-04-19 00:22:13 +0100

    set up a custom property and used @Input decorator to pass the details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [
+    { type: 'server', name: 'Testserver', content: 'just a test server!' },
+  ];
 }
